c7019f88e087725ca40a20cff4726f55ea884a0a
nachoKDev <nachokorkk@gmail.com>
2025-12-16 11:09:32 +0100

    CI added for push docker image

--- src/app.py
+++ src/app.py
@@ -1,18 +1,15 @@
-# '/api/v1/detailts'
-# '/api/v1/healthz'
-
 from flask import Flask, jsonify
+import datetime
+import socket
 
 app = Flask(__name__)
 
 @app.route('/api/v1/details', methods=['GET'])
-def get_details():
-    details = {
-        "name": "Example API",
-        "version": "1.0.0",
-        "description": "This is an example API endpoint providing details."
-    }
-    return jsonify(details), 200
+def details():
+    return jsonify({
+        'time' : datetime.datetime.now().strftime("%U:%M:%S%p on %A, %B %d, %Y"),
+        'hostname': socket.gethostname()
+    })
 
 @app.route('/api/v1/healthz', methods=['GET'])
 def health_check():
@@ -20,12 +17,6 @@ def health_check():
         "status": "healthy",
     }
     return jsonify(health_status), 200
-
-@app.route('/api/v1/hostname', methods=['GET'])
-def get_hostname():
-    import socket
-    hostname = socket.gethostname()
-    return jsonify({"hostname": hostname}), 200
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
