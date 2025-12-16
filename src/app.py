104355a06c0f3f80d52ebe0cabbd0a6e268d3962
nachoKDev <nachokorkk@gmail.com>
2025-12-16 18:27:29 +0100

    modify values on cd stepv2

--- src/app.py
+++ src/app.py
@@ -14,10 +14,9 @@ def details():
 @app.route('/api/v1/healthz', methods=['GET'])
 def health_check():
     health_status = {
-        "status": "Up and Running",
+        "status": "Up and Running!",
     }
     return jsonify(health_status), 200
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
-
